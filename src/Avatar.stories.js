2bc0ad97598f6551b5036de862c1f1d882688ca9
Devin Amarasekara <71481145+devinsanduka@users.noreply.github.com>
2022-06-27 19:06:34 +0530

    second commit

--- src/Avatar.stories.js
+++ src/Avatar.stories.js
@@ -5,6 +5,15 @@ import { Avatar } from "./Avatar";
 export default {
   title: "Design System/Avatar",
   component: Avatar,
+
+  argTypes: {
+    size: {
+      control: {
+        type: 'select',
+      },
+      options: ['tiny', 'small', 'medium', 'large'],
+    },
+  },
 };
 
 export const Standard = (args) => <Avatar {...args} />;
@@ -59,3 +68,14 @@ export const Large = (args) => (
     />
   </div>
 );
+
+const Template = args => <Avatar {...args} />;
+
+export const Controls = Template.bind({});
+
+Controls.args = {
+  loading: false,
+  size: 'tiny',
+  username: 'Dis Boi',
+  src: 'https://i1.sndcdn.com/avatars-000427496508-zdws3r-t500x500.jpg',
+}
--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import { userEvent, within } from '@storybook/testing-library';
 import { Button } from './Button';
 import { Icon } from './Icon';
 import { StoryLinkWrapper } from './StoryLinkWrapper';
@@ -290,3 +290,18 @@ export const AnchorWrapper = (args) => (
 );
 
 AnchorWrapper.storyName= 'anchor wrapper';
+
+export const WithInteractions = () => (
+  <Button
+    ButtonWrapper={StoryLinkWrapper}
+    appearance="priimary"
+    href="http://storybook.js.org">
+      Button
+    </Button>
+);
+WithInteractions.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  await userEvent.click(canvas.getByRole("link"));
+};
+
+WithInteractions.storyName = "button with interactions";
