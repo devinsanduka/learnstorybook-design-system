b9b533b62065b09e051ec5660f16b030d7e348d5
Devin Amarasekara <71481145+devinsanduka@users.noreply.github.com>
2022-07-01 17:59:57 +0530

    Documentation

--- src/Avatar.stories.js
+++ src/Avatar.stories.js
@@ -14,6 +14,10 @@ export default {
       options: ['tiny', 'small', 'medium', 'large'],
     },
   },
+
+  parameters: {
+    componentSubtitle: 'Displays an image that represents a user or organization',
+  },
 };
 
 export const Standard = (args) => <Avatar {...args} />;
@@ -34,6 +38,13 @@ export const Sizes = (args) => (
 Sizes.args = {
   username: "Tom Coleman",
   src: "https://avatars2.githubusercontent.com/u/132554",
+};
+
+Sizes.parameters = {
+  docs: {
+  // The story now contains a description
+  storyDescription: '4 sizes are supported.',
+  },
 };
 
 export const Initials = (args) => (
